480bdf7e1f441ebbf78c0547cd76fed64bbe675e
wqshao <wangquan.shao@ingenic.com>
2016-09-13 19:20:48 +0800

    add ring buffer

--- C/buffer/ring_buffer.c
+++ C/buffer/ring_buffer.c
@@ -23,7 +23,7 @@ static int init_cycle_buffer(void)
 {
 	int size = BUFFSIZE, ret;
 
-	ret = size & (size - 1); //size = 2^x 
+	ret = size & (size - 1); //size = 2^x
 	if (ret)
 		return ret;
 
@@ -133,5 +133,11 @@ int main(void)
 
 	return 0;
 }
+
+//1buffer指向存放数据的缓冲区，size是缓冲区的大小，in是写指针下标，out是读指针下标,在len和(fifo->size - fifo->in + fifo->out)之间取一个较小的值赋给len。注意，当(fifo->in == fifo->out+fifo->size)时，表示缓冲区已满，此时得到的较小值一定是0，后面实际写入的字节数也全为0。另一种边界情况是当len很大时（因为len是无符号的，负数对它来说也是一个很大的正数），这一句也能保证len取到一个较小的值，因为fifo->in总是大于等于fifo->out， 所以后面的那个表达式的值不会超过fifo->size的大小把上一步决定的要写入的字节数len“切开”，这里又使用了一个技巧。注意：实际分配 给fifo->buffer的字节数fifo->size，必须是2的幂，否则这里就会出错。既然fifo->size是2的幂，那么 (fifo->size-1)也就是一个后面几位全为1的数，也就能保证(fifo->in & (fifo->size - 1))总为不超过(fifo->size - 1)的那一部分，和(fifo->in)% (fifo->size - 1)的效果一样。
+
+//这样后面的代码就不难理解了，它先向fifo->in到缓冲区末端这一块写数据，如果还没写完，在从缓冲区头开始写入剩下的，从而实现了循环缓冲。最后，把写指针后移len个字节，并返回len。
+
+//从上面可以看出，fifo->in的值可以从0变化到超过fifo->size的数值，fifo->out也如此，但它们的差不会超过fifo->size
 
 
