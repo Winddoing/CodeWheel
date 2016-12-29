db4bfd01fb7e025c3442041779f444d05133d3ea
wqshao <wangquan.shao@ingenic.com>
2016-12-29 15:10:12 +0800

    add modify

--- tools/git_file_check.sh
+++ tools/git_file_check.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+PWD=`pwd`
+
 git ls-tree -r --name-only HEAD | while read filename; do
-  echo "$(git log -1 --format="%an %ae" -- $filename) $filename"
+  echo -e "$(git log -1 --format="\e[1;33m [%an] \e[0m:%ae" -- $filename) \e[1;31m File \e[0m:$PWD/$filename"
   done
