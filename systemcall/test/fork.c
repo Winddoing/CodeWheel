686553e376de54fe9cde665755719e9467aa71e5
wqshao <shaowangquan@gmail.com>
2017-03-07 01:09:18 +0800

    add process exit code

--- systemcall/test/fork.c
+++ systemcall/test/fork.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <errno.h>
 
 int main(void)
 {
 	int count = 1;
 	int pid;
+	int status;
+	int *a;
 
 	pid = fork( );
-
 	printf("pid=%d\n", pid);
+
 	if(pid < 0) {
 		perror("fork error : ");
 	} else if(pid == 0) {
-		printf("This is son, his count is: %d (%p). and his pid is: %d\n", ++count, &count, getpid());
+		printf("This is son, his count is: %d (%p). and his pid is: %d\n", 
+				++count, &count, getpid());
+		*a = 3;
+		sleep(10);
+		_exit(0);
 	} else {
-		printf("This is father, his count is: %d (%p), his pid is: %d\n", count, &count, getpid());
+		pid = wait(&status);
+
+		printf("This is father, his count is: %d (%p), his pid is: %d, son exit status: %d[%08x]\n", 
+				count, &count, getpid(), status, status);
 	}
 
 	return 0;
