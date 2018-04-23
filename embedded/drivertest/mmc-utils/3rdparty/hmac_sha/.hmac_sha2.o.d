efe1312f622e3565721f3216dcd3fcf1dd27bc49
wqshao <wangquan.shao@ingenic.com>
2018-04-23 14:21:25 +0800

    [Add] add mmc utils

---
+++ embedded/drivertest/mmc-utils/3rdparty/hmac_sha/.hmac_sha2.o.d
@@ -0,0 +1,2 @@
+3rdparty/hmac_sha/hmac_sha2.o: 3rdparty/hmac_sha/hmac_sha2.c \
+ 3rdparty/hmac_sha/hmac_sha2.h 3rdparty/hmac_sha/sha2.h
---
+++ embedded/drivertest/mmc-utils/3rdparty/hmac_sha/.sha2.o.d
@@ -0,0 +1,2 @@
+3rdparty/hmac_sha/sha2.o: 3rdparty/hmac_sha/sha2.c \
+ 3rdparty/hmac_sha/sha2.h
