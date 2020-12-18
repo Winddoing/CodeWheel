d0fabd398e5bc02ee69825859ce87a849dfa0980
winddoing <winddoing@sina.cn>
2020-12-18 18:17:33 +0800

    add rust test

---
+++ rust/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##########################################################
+# File Name		: env.sh
+# Author		: winddoing
+# Created Time	: 2020年11月06日 星期五 09时20分22秒
+# Description	:
+##########################################################
+
+sudo apt install rustc
---
+++ rust/m.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##########################################################
+# File Name		: m.sh
+# Author		: winddoing
+# Created Time	: 2020年11月06日 星期五 09时22分01秒
+# Description	:
+##########################################################
+
+rustc hello.rs -o hello.out
