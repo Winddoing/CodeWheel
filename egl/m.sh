288d5bf694d36878d3639ce3899687644ff2bc2a
winddoing <winddoing@sina.cn>
2021-03-03 17:02:34 +0800

    delete useless code

--- egl/m.sh
+++ egl/m.sh
@@ -17,8 +17,4 @@ gcc egl_off-scree_rendering.c -o egl_off-scree_rendering.out `pkg-config --libs 
 gcc demo.c -o demo.out -g `pkg-config --libs --cflags egl gbm gl libdrm`
 
 
-
-gcc ./xeglthreads.c -o ./xeglthreads.out -g `pkg-config --libs --cflags egl gbm gl libdrm x11` -lpthread -lm
-
-
 gcc egl_gbm_render.c -o egl_gbm_render.out `pkg-config --libs --cflags egl gl gbm`
