1d6dd368a6b9e4c94da921f6bbaa1c45b4f85073
winddoing <winddoing@sina.cn>
2021-04-20 10:20:48 +0800

    fix

--- socket/test/check_ip.c
+++ socket/test/check_ip.c
@@ -13,27 +13,33 @@
 
 static int is_ip_valid(char* check_ip)
 {
-    char* net_segment_ip = "192.168.2.0";
-    char* net_segment_mark = "255.255.255.0";
+    char* whitelist_address_seg = "192.168.2.0";
+    //char* whitelist_address_seg = "0.0.0.0";
+    char* whitelist_netmark     = "255.255.255.0";
 
-    struct in_addr nseg_ip_addr, nseg_mark_addr, check_ip_addr;
+    struct in_addr nseg_addr, nseg_netmark, check_addr;
 
-    inet_pton(AF_INET, net_segment_ip, (void *)&nseg_ip_addr);
-    inet_pton(AF_INET, net_segment_mark, (void *)&nseg_mark_addr);
-    inet_pton(AF_INET, check_ip, (void *)&check_ip_addr);
-
-    
-    printf("===> func: %s, line: %d\n, nseg_ip:%x, nseg_mark:%x, ip:%x\n", __func__, __LINE__, nseg_ip_addr.s_addr, nseg_mark_addr.s_addr, check_ip_addr.s_addr);
+    inet_pton(AF_INET, whitelist_address_seg, (void *)&nseg_addr);
+    inet_pton(AF_INET, whitelist_netmark, (void *)&nseg_netmark);
+    inet_pton(AF_INET, check_ip, (void *)&check_addr);
 
     
-    printf("===> func: %s, line: %d, ----:%x\n", __func__, __LINE__, nseg_mark_addr.s_addr & check_ip_addr.s_addr);
-    printf("===> func: %s, line: %d, --|--:%x\n", __func__, __LINE__, (nseg_mark_addr.s_addr & check_ip_addr.s_addr) | nseg_ip_addr.s_addr);
-    printf("===> func: %s, line: %d, --^--:%x\n", __func__, __LINE__, nseg_mark_addr.s_addr & check_ip_addr.s_addr ^ nseg_ip_addr.s_addr);
+    printf("===> func: %s, line: %d\n, nseg_ip:%x, nseg_mark:%x, ip:%x\n", __func__, __LINE__, nseg_addr.s_addr, nseg_netmark.s_addr, check_addr.s_addr);
 
-    if ((nseg_mark_addr.s_addr & check_ip_addr.s_addr) ==  nseg_ip_addr.s_addr)
+    
+    printf("===> func: %s, line: %d, ----:%x\n", __func__, __LINE__, nseg_netmark.s_addr & check_addr.s_addr);
+    printf("===> func: %s, line: %d, ----:%x\n", __func__, __LINE__, nseg_netmark.s_addr & nseg_addr.s_addr);
+    printf("===> func: %s, line: %d, --|--:%x\n", __func__, __LINE__, (nseg_netmark.s_addr & check_addr.s_addr) | nseg_addr.s_addr);
+    printf("===> func: %s, line: %d, --^--:%x\n", __func__, __LINE__, nseg_netmark.s_addr & check_addr.s_addr ^ nseg_addr.s_addr);
+
+    printf("===> func: %s, line: %d, nseg_addr.s_addr:%x\n", __func__, __LINE__, nseg_addr.s_addr);
+
+    if (!(nseg_addr.s_addr & nseg_netmark.s_addr) ||
+            (nseg_netmark.s_addr & check_addr.s_addr) ==  nseg_addr.s_addr) {
         return 1;
-    else
+    } else {
         return 0;
+    }
 }
 
 int main(int argc, const char *argv[])
@@ -43,5 +49,8 @@ int main(int argc, const char *argv[])
     ret = is_ip_valid("192.168.1.2");
     printf("===> func: %s, line: %d, ret=%d\n", __func__, __LINE__, ret);
     
+    ret = is_ip_valid("192.168.2.2");
+    printf("===> func: %s, line: %d, ret=%d\n", __func__, __LINE__, ret);
+
     return 0;
 }
