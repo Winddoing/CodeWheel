707960e0ab62718c4a02db8971a146582532ccd1
winddoing <winddoing@sina.cn>
2021-07-22 10:29:31 +0800

    update

--- C/numa/test_numa.c
+++ C/numa/test_numa.c
@@ -11,22 +11,20 @@
 
 #include <numa.h>
 
-/**
- * @brief  set_numa_node 将当前进程绑定到第node个numa节点上
- *
- * @param node  numa节点号，比如两个CPU有两个numa节点即0或者1
- *
- * @returns   
- */
+/**                                                            
+ * @brief  set_numa_node 将当前进程绑定到第node个numa节点上    
+ *                                                             
+ * @param node  numa节点号，比如两个CPU有两个numa节点即0或者1  
+ *                                                             
+ * @returns                                                    
+ */                                                            
 static int set_numa_node(int node)
 {
     struct bitmask *bit;
-    int numa_max_num = 0;
     int numa_node_num = 0;
 
-    numa_max_num = numa_max_node();
-    numa_node_num = numa_max_num + 1;
-    printf("numa max num=%d, node num=%d\n", numa_max_num, numa_node_num);
+    numa_node_num = numa_num_configured_nodes();
+    printf("numa node num=%d\n", numa_node_num);
 
     bit = numa_bitmask_alloc(numa_node_num);
     numa_bitmask_clearall(bit);
@@ -40,13 +38,14 @@ static int set_numa_node(int node)
 
 int main(int argc, const char *argv[])
 {
+
     if(numa_available() < 0){
         printf("your system does not support numa api\n");
         return -1;
     }
 
     //set_numa_node(0);
-    set_numa_node(0);
+    set_numa_node(1);
 
     printf("pid = %d\n", getpid());
     while(1);
